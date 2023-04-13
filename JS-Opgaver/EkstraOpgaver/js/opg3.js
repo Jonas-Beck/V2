08255227a651dfc1061546c5acfe979901b62ed1
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-04-13 11:26:31 +0200

    Opgave 3

--- JS-Opgaver/EkstraOpgaver/js/opg3.js
+++ JS-Opgaver/EkstraOpgaver/js/opg3.js
@@ -1,1 +1,9 @@
 // Løs opgave 3 ved at udfylde denne js-fil med variabler, event-listners og functions
+const wrapper = document.querySelector("#wrapper")
+
+document.querySelector("#knap").addEventListener("click", function(event){
+    wrapper.style.backgroundColor = "lightgreen";
+    event.target.style.backgroundColor = "blue"
+    event.target.style.color = "white"
+    event.target.disabled = "true"
+}, false)
