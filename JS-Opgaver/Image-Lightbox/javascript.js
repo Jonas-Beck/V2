4e486089c40644363369b09598c295255fe4658f
Jonas-Beck <jona63m2@edu.campusvejle.dk>
2023-04-20 09:53:31 +0200

    Light Box Update

--- JS-Opgaver/Image-Lightbox/javascript.js
+++ JS-Opgaver/Image-Lightbox/javascript.js
@@ -7,7 +7,7 @@ document.querySelectorAll("#gallery img").forEach((item) =>
         const img = document.createElement("img")
         img.src = e.target.src.replace("thumb_", "")
         div.appendChild(img)
-        div.addEventListener("click", function(e){
+        div.addEventListener("click", function () {
             div.remove()
         })
         body.appendChild(div)
